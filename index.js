67bc751bcd8127a8b80c4ea821aaf8f5ab1c9f53
Stephanie Andrews <byronic.string@gmail.com>
2020-05-16 10:36:56 -0700

    important port line for deploy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app     = express();
-const PORT    = 3000;
+const PORT    = process.env.PORT || 3000;
 const server  = require('http').createServer(app);
 const io      = require('socket.io')(server);
 
