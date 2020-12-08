86c932201b513eb281e6725d18c8f45009227e67
Apoorva Rao Balevalachilu <apoorva.rao.b@gmail.com>
2020-12-07 22:31:25 -0800

    adds author/artist details

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -87,16 +87,17 @@ function setup() {
   //wordAcquired.text('new word', 100, 150); 
   
   let title = createP('Early Words');
-  let shortIntro = createP('Early words is an abstract model of the fascinating language acquisition process of 18 to 36 month olds. It aims to cast light on word comprehension and production. The data used is intentionally cross-linguistic to serve as a reminder that multiple languages can be acquired simultaneously in the same abstract space of the mind and be enriched by it. ');
+  let shortIntro = createP('Early Words is an abstract model of the fascinating language acquisition process of 18 to 36 month olds. It aims to cast light on word comprehension and production. The data used is intentionally cross-linguistic to serve as a reminder that multiple languages can be acquired simultaneously in the same abstract space of the mind and be enriched by it. ');
   let description = createP('This piece is an encounter with human first sounds and words. The words displayed are filtered by age, but not by language.  Eggs hatch to reveal words acquired by the hypothetical multilingual toddler. The words occupy a space above the grassy globe made out of the sounds of babbling. Today, out of approximately seven thousand known languages, only a handful are dominant. Words from all languages are valued equally in this abstract space.'); 
   let dataSource = createP('Primary data source: Frank, M. C., Braginsky, M., Yurovsky, D., & Marchman, V. A. (2016). Wordbank: An open repository for developmental vocabulary data. Journal of Child Language. doi: 10.1017/S0305000916000209.');
   let howToInteract = createP('The animation will begin as soon as you open the window. To interact with the app, use your mouse pointer to change the direction of a point light source. Move the mouse pointer horizontally to step away from and towards the scene. Click the play button to hear babbling and watch the globe change in size according to the volume.');
-
+  let artist = createP('Artist: Apoorva Rao Balevalachilu for the GrayArea Winter 2019 Showcase.');
   title.class('title');
   shortIntro.class('artInfo');
   description.class('artInfo');
   dataSource.class('artInfo');
   howToInteract.class('artInfo');
+  artist.class('artInfo');
 
   //OLD version
   //URL: https://shrouded-woodland-95521.herokuapp.com. 
