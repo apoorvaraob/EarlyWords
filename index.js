c6266cc480ff33d75964b4c225cb89d16d2ca0aa
Apoorva Rao Balevalachilu <apoorva.rao.b@gmail.com>
2021-01-12 19:27:58 -0800

    removes unused variable io

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app     = express();
 const PORT    = process.env.PORT || 5000;
 const server  = require('http').createServer(app);
-const io      = require('socket.io')(server);
 const path = require("path");
 const router = express.Router();
 
